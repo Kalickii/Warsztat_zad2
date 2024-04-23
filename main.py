fde80c60fca2671797ccd5fd6c8c737afbfc823e
Michał <michal.kalicki@outlook.com>
2024-04-23 12:11:59 +0200

    Creating user input list

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ for i in range(6):
     if x not in lotto:
         lotto.append(x)
 
+while len(guess) < 6:
+    try:
+        user_number = int(input("Enter 6 correct numbers from 1 to 49 to win: "))
+        if user_number > 49:
+            print("The number is too big!")
+        elif user_number < 1:
+            print("The number is too small!")
+        elif user_number in guess:
+            print("You have already picked that number!")
+        else:
+            guess.append(user_number)
+    except ValueError:
+        print("Please enter only numbers!")
